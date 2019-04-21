5a10606572f2da3e339456a90fc27bed4da985db
theprankmonkey <matthew.hellmer@ntrepidcorp.com>
2019-04-21 02:04:31 -0400

    Updated tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_sonarr_service(host):
+    host.service('sonarr').is_running
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_sonarr_page(host):
+    cmd = host.run("curl 127.0.0.1:8989")
+    cmd.rc == 0
+    "Sonarr" in cmd.stdout
